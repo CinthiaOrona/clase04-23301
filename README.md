8eb27a7fa3fedfbd298f45a0a001d1234ccb76a2
Cinthia M. Orona <devrider2021@gmail.com>
2023-03-12 03:29:41 -0300

    Practica clases y objetos Tarea 4

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# clase04-23301
+## **clase04-23301**
+
+
+# Ejercicio
+
+>* Completar la class buton :heavy_check_mark:
+>* Que se dibuje un boton en html luego de llamar al render.  :heavy_check_mark:	
+>* Van a crear otra class input/text o el que sea y que se dibuje igual que boton en el html.  :heavy_check_mark:
---
+++ js/app.js
@@ -0,0 +1,121 @@
+/********** TAREA CLASE 04 *****************/
+
+/*******************INICIO*****************/
+class App{
+    constructor(parentID, className){
+        this.parentID = parentID;
+        this.className = className;
+    }
+
+    render(){
+        let myApp = document.getElementById(this.parentID);
+        myApp.className = this.className;
+
+    }
+}
+
+class Input {
+    constructor(parentID, type, placeholder, className, inputID) {
+        this.parentID = parentID;
+        this.type = type;
+        this.placeholder = placeholder;
+        this.className = className;
+        this.inputID = inputID;
+    }
+
+    render() {
+        const myApp = document.getElementById(this.parentID);
+        const input = document.createElement("input");
+        //myApp.innerHTML = `<input className="${this.className}" type="${this.type}" placeholder="${this.placeholder}">`;
+        input.type = this.type;
+        input.placeholder = this.placeholder;
+        input.className = this.className;
+        myApp.append(input);
+    }
+}
+
+
+class Button {
+    constructor(parentID, text, type, className, buttonID) {
+        this.parentID = parentID;
+        this.text = text;
+        this.type = type;
+        this.className = className;
+        this.buttonID = buttonID;
+    }
+
+    render() {
+        const myApp = document.getElementById(this.parentID);
+        const button = document.createElement("button");
+        button.textContent = this.text;
+        button.type = this.type;
+        button.className = this.className;
+        myApp.append(button);  
+        
+        button.addEventListener('click', (e) =>{
+            e.preventDefault();
+            const myInput = document.querySelector("input");
+            let mensaje = document.createElement("div");
+            mensaje.setAttribute("id", "texto");
+            mensaje.className = "alert alert-success m-4";
+            mensaje.textContent = myInput.value;
+            document.body.append(mensaje);
+        });
+    }    
+}
+
+const myApp = new App("app", "container m-4 p-4 bg-light");
+myApp.render();
+
+const myInput = new Input("app", "text", "Escribe tu nombre", "form-control md-5", "input");
+myInput.render();
+
+const myButton = new Button("app", "Click me!", "button", "btn btn-primary", "button");
+myButton.render();
+
+
+/*********************FIN**********************/
+
+/********** PRACTICA DE CLASE 04 **************/
+
+let myDom = document; // document is a global object
+
+const myDog = {
+    name: "Firulais"
+};
+
+const myCat = {
+    "name": "Kity"
+};
+
+
+console.log(myDog.name);
+console.log(myCat.name);
+
+// Como "clonar otro perro". Una plantilla.
+
+class Perro{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+}
+
+
+const cookie = new Perro("Cookie", 5);
+//cookie.render();
+
+const pichicho = new Perro("Firu", 3);
+//console.log(pichicho);
+//pichicho.render();
+
+
+// function Perro(name, age){
+//     this.name = name;
+//     this.age = age;
+// }
+
+const rocco = new Perro("Roquito", 9);
+//rocco.render();
+
+
